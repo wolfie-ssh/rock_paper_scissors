870d86d3894e9e2c13512aba58c69eb2825359aa
wolfie-ssh <tilakg0p1605@gmail.com>
2024-10-11 19:05:34 +0530

    add logic for getComputerChoice() function

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 function getComputerChoice() {
-    let RandomChoice = Math.round(Math.random() * 100);
-    console.log(RandomChoice);
+    let RandomNumber = Math.random();
+    let Choice;
+
+    if (RandomNumber < (1 / 3))
+    {
+        Choice="Rock"; // 0 to ~0.33
+    }
+    else if (RandomNumber < (2 / 3)){
+        Choice = "Paper"; // ~0.33 to ~0.67
+    }
+    else {
+        Choice = "Scissors"; // ~0.67 to 1
+    }
+    console.log(Choice);
 }
 
 getComputerChoice();
